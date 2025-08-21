451c23997a8158fc7557a375d68d75ae3f770613
Talysson Decol <Talyssonlago@gmail.com>
2025-08-21 16:08:44 -0300

    (feat): implementar rolagens de dados

---
+++ rpg/dados.py
@@ -0,0 +1,19 @@
+import random
+from typing import List
+
+class Dado:
+    @staticmethod
+    def rolar(lados: int, vezes: int) -> List[int]:
+        return [random.randint(1, lados) for _ in range(vezes)]
+    
+    @staticmethod
+    def rolar_soma(lados: int, vezes: int) -> int:
+        return sum(Dado.rolar(lados, vezes))
+    
+    @staticmethod
+    def rolar_4d6_drop_lowest() -> int:
+        rolagens = Dado.rolar(6, 4)
+        rolagens.remove(min(rolagens))
+        return sum(rolagens)
+                   
+                   
